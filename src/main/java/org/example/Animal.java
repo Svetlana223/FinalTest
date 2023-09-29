5d28c2ccaa417cc4a20a033c96342094272a277e
Svetlana <strel_sv@bk.ru>
2023-09-28 21:42:58 -0400

    добавлен реестр

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Animal {
     private String name;
+    private List<String> commands = new ArrayList<>();
 
     public Animal(String name) {
         this.name = name;
@@ -9,5 +13,13 @@ public class Animal {
 
     public String getName() {
         return name;
+    }
+
+    public void addCommand(String command) {
+        commands.add(command);
+    }
+
+    public List<String> getCommands() {
+        return commands;
     }
 }
